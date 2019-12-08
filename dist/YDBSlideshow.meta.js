a83c66ab5b0ce6d7a56463baabefccbf39cd683a
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2019-12-08 16:28:35 +0100

    Bump packages

--- dist/YDBSlideshow.meta.js
+++ dist/YDBSlideshow.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        YDBSlideshow
-// @version     "0.4.1"
+// @version     "0.4.2"
 // @author      theguy159
 // @description An YDB plugin for automatic slideshow on derpibooru.org
 // @homepage    https://tehvoid.net
