cc290be52bbfc654c1b8690f7a65f91f212849a6
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2020-06-08 17:41:18 +0200

    build

--- dist/YDBSlideshow.meta.js
+++ dist/YDBSlideshow.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        YDBSlideshow
-// @version     "0.4.6"
+// @version     "0.4.7"
 // @author      theguy159
 // @description An YDB plugin for automatic slideshow on derpibooru.org
 // @homepage    https://tehvoid.net
